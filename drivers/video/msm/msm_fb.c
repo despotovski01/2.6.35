f2cfede1821cebd6e87c0abf6b66cbfc3068664a
francisco <francisco@francisco-VPCEB1M1E.(none)>
2011-07-02 21:21:28 +0100

    black screen fixed

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -679,10 +679,18 @@ static void msmfb_early_resume(struct early_suspend *h)
 						    early_suspend);
 	msm_fb_resume_sub(mfd);
 }
-
+#if CONFIG_LGE_GRAM_REFRESH_PATCH
+static void msmfb_early_suspend_early(struct early_suspend *h)
+{
+  /* do nothing */
+}
+	
+static void msmfb_late_resume_late(struct early_suspend *h)
+{
 	memset((void *)last_info->screen_base, 0, last_info->fix.smem_len);
 	msm_fb_pan_display(last_var, last_info);
 }
+#endif
 #endif
 
 void msm_fb_set_backlight(struct msm_fb_data_type *mfd, __u32 bkl_lvl)
@@ -1224,7 +1232,12 @@ static int msm_fb_register(struct msm_fb_data_type *mfd)
 		mfd->early_suspend.level = EARLY_SUSPEND_LEVEL_DISABLE_FB - 2;
 		register_early_suspend(&mfd->early_suspend);
 	}
-
+#if CONFIG_LGE_GRAM_REFRESH_PATCH
+	additional_early_suspend.suspend = msmfb_early_suspend_early;
+	additional_early_suspend.resume = msmfb_late_resume_late;
+	additional_early_suspend.level = EARLY_SUSPEND_LEVEL_DISABLE_FB - 10;
+	register_early_suspend(&additional_early_suspend);
+#endif
 #endif
 
 #ifdef MSM_FB_ENABLE_DBGFS
@@ -1427,8 +1440,8 @@ static int msm_fb_pan_display(struct fb_var_screeninfo *var,
 	struct mdp_dirty_region *dirtyPtr = NULL;
 	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)info->par;
 
-	last_var = var;
-	last_info = info;
+	//last_var = var;
+	//last_info = info;
 
 	if ((!mfd->op_enable) || (!mfd->panel_power_on))
 		return -EPERM;
--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 58 */
+/* This file is auto generated, version 60 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#58 PREEMPT Sat Jul 2 20:14:10 WEST 2011"
-#define LINUX_COMPILE_TIME "20:14:10"
+#define UTS_VERSION "#60 PREEMPT Sat Jul 2 21:14:35 WEST 2011"
+#define LINUX_COMPILE_TIME "21:14:35"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "francisco-VPCEB1M1E"
 #define LINUX_COMPILE_DOMAIN "(none)"
