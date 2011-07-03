8b3b501ae6fdab9812e6dff0524558d1f9ee54f7
francisco <francisco@francisco-VPCEB1M1E.(none)>
2011-07-03 02:36:57 +0100

    Added BFS 404 for testing purposes

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 60 */
+/* This file is auto generated, version 64 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#60 PREEMPT Sat Jul 2 21:14:35 WEST 2011"
-#define LINUX_COMPILE_TIME "21:14:35"
+#define UTS_VERSION "#64 PREEMPT Sun Jul 3 02:22:46 WEST 2011"
+#define LINUX_COMPILE_TIME "02:22:46"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "francisco-VPCEB1M1E"
 #define LINUX_COMPILE_DOMAIN "(none)"
