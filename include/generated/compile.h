e60a482b3955cbf974a9b36ffffd1db5c47198ee
francisco <francisco@francisco-VPCEB1M1E.(none)>
2011-07-03 03:53:52 +0100

    Bye debugs and tweaked BFS

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 64 */
+/* This file is auto generated, version 66 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#64 PREEMPT Sun Jul 3 02:22:46 WEST 2011"
-#define LINUX_COMPILE_TIME "02:22:46"
+#define UTS_VERSION "#66 PREEMPT Sun Jul 3 03:48:40 WEST 2011"
+#define LINUX_COMPILE_TIME "03:48:40"
 #define LINUX_COMPILE_BY "lg-electronics"
 #define LINUX_COMPILE_HOST "francisco-VPCEB1M1E"
 #define LINUX_COMPILE_DOMAIN "(none)"
