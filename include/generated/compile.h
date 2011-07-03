6e643f43ff0d713780a4acb59630f513cfa04618
francisco <francisco@francisco.(none)>
2011-07-03 22:15:15 +0100

    time: Compensate for rounding on odd-frequency clocksources
    commit a386b5af8edda1c742ce9f77891e112eefffc005 upstream.
    
    When the clocksource is not a multiple of HZ, the clock will be off.  For
    acpi_pm, HZ=1000 the error is 127.111 ppm:
    
    The rounding of cycle_interval ends up generating a false error term in
    ntp_error accumulation since xtime_interval is not exactly 1/HZ.  So, we
    subtract out the error caused by the rounding.
    
    This has been visible since 2.6.32-rc2
    	commit a092ff0f90cae22b2ac8028ecd2c6f6c1a9e4601
    	time: Implement logarithmic time accumulation
    That commit raised NTP_INTERVAL_FREQ and exposed the rounding error.
    
    testing tool: http://n1.taur.dk/permanent/testpmt.c
    Also tested with ntpd and a frequency counter.
    
    Signed-off-by: Kasper Pedersen <kkp2010@kasperkp.dk>
    Acked-by: john stultz <johnstul@us.ibm.com>
    Cc: John Kacur <jkacur@redhat.com>
    Cc: Clark Williams <williams@redhat.com>
    Cc: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Will Tisdale <willtisdale@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    
    Conflicts:
    
    X	kernel/time/timekeeping.c

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-/* This file is auto generated, version 66 */
+/* This file is auto generated, version 1 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#66 PREEMPT Sun Jul 3 03:48:40 WEST 2011"
-#define LINUX_COMPILE_TIME "03:48:40"
-=======
-/* This file is auto generated, version 60 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#60 PREEMPT Sat Jul 2 21:14:35 WEST 2011"
-#define LINUX_COMPILE_TIME "21:14:35"
->>>>>>> parent of 8b3b501... Added BFS 404 for testing purposes
+#define UTS_VERSION "#1 PREEMPT Sun Jul 3 21:37:46 WEST 2011"
+#define LINUX_COMPILE_TIME "21:37:46"
 #define LINUX_COMPILE_BY "lg-electronics"
-#define LINUX_COMPILE_HOST "francisco-VPCEB1M1E"
+#define LINUX_COMPILE_HOST "francisco"
 #define LINUX_COMPILE_DOMAIN "(none)"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.35.13-franco.Kernel.v1-GINGERBREAD-PRE.RELEASE+"
+#define UTS_RELEASE "2.6.35.13-franco.Kernel.v1-GINGERBREAD+"
